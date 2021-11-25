bf055d863aaad918017871a87a5eeb61a92f4d28
Lasse Larsen <justlasse@gmail.com>
2021-11-25 12:23:38 +0100

    Fixed tests

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::whereHas('projects', function (Builder $query) {
+            $query->where('name', '!=', null);
+        }, '>=', 1)->get();
 
         return view('users.index', compact('users'));
     }
