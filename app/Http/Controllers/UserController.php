e47f8416e60f0e0abf1e53587374aded0400fc1a
gbemiro <peculiar.isrl17@gmail.com>
2021-11-25 22:24:04 +0100

    fix failing tests

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all()->whereNotNull('project_id');
+        $users = User::all()->whereNotNull('projects');
 
         return view('users.index', compact('users'));
     }
