ce75a1a06f91a59d0b25be159295915b21e4ba0b
veerman72 <64311955+veerman72@users.noreply.github.com>
2021-11-25 08:23:35 +0100

    Task 3

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,7 +49,7 @@ class User extends Authenticatable
 
     public function comments()
     {
-        // TASK: add the code here for two-level relationship
+        return $this->hasManyThrough(Comment::class, Task::class, 'users_id', 'task_id', 'id', 'id');
     }
 
     public function projects()
