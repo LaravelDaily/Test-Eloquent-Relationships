d846d75ddd34caf3f00b26cc50409927086c52ff
guus-frenken <g.frenken@qteco.nl>
2021-11-25 09:59:01 +0100

    All tasks completed

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -10,6 +10,10 @@ class ProjectController extends Controller
     {
         // TASK: Add one sentence to save the project to the logged-in user
         //   by $request->project_id and with $request->start_date parameter
+        auth()->user()->projects()
+            ->attach($request->project_id, [
+                'start_date' => $request->start_date
+            ]);
 
         return 'Success';
     }
