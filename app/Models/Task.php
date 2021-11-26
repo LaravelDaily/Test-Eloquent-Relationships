297ce2474e4f581d541f80baae6285070dde1e0d
jwmcross <59067337+jwmcross@users.noreply.github.com>
2021-11-26 12:47:02 +0000

    Task #2 - Added withDefault to relationsgip for missing values

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -13,6 +13,7 @@ class Task extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'users_id');
+        return $this->belongsTo(User::class, 'users_id')
+            ->withDefault();
     }
 }
