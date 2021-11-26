79579e37ff96da141fbcfbf87a10d741e69dadb6
SusmoySenGupta <susmoy.cse@gmail.com>
2021-11-26 12:08:53 +0600

    feat: add all relationship tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,17 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
     use HasFactory;
 
+    /**
+     * @var array
+     */
     protected $fillable = ['name', 'users_id'];
 
-    public function user()
+    public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'users_id');
+        return $this->belongsTo(User::class, 'users_id', 'id')
+            ->withDefault();
     }
 }
