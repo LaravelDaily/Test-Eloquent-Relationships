a050811f093ddeca7112b9d69f9e2e4fd2a6cdfe
jwmcross <59067337+jwmcross@users.noreply.github.com>
2021-11-26 13:46:39 +0000

    Task #8 - Completed query to attach relationship data

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use phpDocumentor\Reflection\Project;
 
 class ProjectController extends Controller
 {
@@ -10,6 +11,10 @@ class ProjectController extends Controller
     {
         // TASK: Add one sentence to save the project to the logged-in user
         //   by $request->project_id and with $request->start_date parameter
+        $project = auth()->user()->projects()
+            ->attach($request->project_id,[
+                'start_date' => $request->start_date
+                ]);
 
         return 'Success';
     }
