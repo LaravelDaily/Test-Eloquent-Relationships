41080f875b8cda8a4741a1efc1ca4427792d26ca
Romel Niño Paano <50399801+rpaano@users.noreply.github.com>
2021-11-26 17:03:59 +0800

    Create factory and seeder

---
+++ database/factories/TeamFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TeamFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'size' => $this->faker->numberBetween(1, 100)
+        ];
+    }
+}
