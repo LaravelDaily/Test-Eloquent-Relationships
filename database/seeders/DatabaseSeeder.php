dbe7a9507d65a6f47ff3b411756f27b003af9690
Romel Niño Paano <50399801+rpaano@users.noreply.github.com>
2021-11-26 13:14:12 +0800

    Add to Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
-
-
+        $this->call(UserTableSeeder::class);
+        $this->call(TaskTableSeeder::class);
     }
 }
