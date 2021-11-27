698cf8393e9674f80e3ef986a550819afa9b9f0f
Maxie Wright <maxiewright@gmail.com>
2021-11-27 07:56:27 -0400

    complete 9 eloquent relationship tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
@@ -11,7 +12,7 @@ class Task extends Model
 
     protected $fillable = ['name', 'users_id'];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'users_id');
     }
