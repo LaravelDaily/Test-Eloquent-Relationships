db8fd0ff8e84e39e1460e31bd787557bcbfc0186
Kryztan Santos <kryztan.s@gmail.com>
2021-11-29 18:51:17 +0800

    Complete test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,8 +8,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
-
+        $users = User::has('projects')->get();
         return view('users.index', compact('users'));
     }
 
