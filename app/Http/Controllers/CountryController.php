66c0c434294cf98d3f99c7f950e464dd1d9b349b
Adedaramola Adetimehin <69609058+Adedaramola@users.noreply.github.com>
2021-11-30 23:22:18 +0100

    Update CountryController.php

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -9,7 +9,7 @@ class CountryController extends Controller
     public function index()
     {
         // TASK: load the relationship average of team size
-        $countries = Country::avg(Team::class)->get();
+        $countries = Country::withAvg('teams', 'size')->get();
 
         return view('countries.index', compact('countries'));
     }
