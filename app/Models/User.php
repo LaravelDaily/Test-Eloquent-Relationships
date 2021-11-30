25c432bd62a79471c093f6491b9349e579e8bc55
Yaroslav Kovalenko <yaroslav23@gmail.com>
2021-11-30 11:09:33 +0200

    Made all tests passed

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,12 +45,13 @@ class User extends Authenticatable
     public function tasks()
     {
         // TASK: fix this by adding a parameter
-        return $this->hasMany(Task::class);
+        return $this->hasMany(Task::class, 'users_id', 'id');
     }
 
     public function comments()
     {
         // TASK: add the code here for two-level relationship
+        return $this->hasManyThrough(Comment::class, Task::class, 'users_id', 'task_id');
     }
 
     public function projects()
