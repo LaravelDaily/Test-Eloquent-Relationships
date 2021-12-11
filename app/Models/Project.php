8f8ed0bdd2f48c00658ac69cc9a1ac7b3ed200bc
rhfahad <riaz.hossain.fahad@gmail.com>
2021-12-11 13:34:17 +0600

    fix: all the tests are passed

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Project extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    /**
+     * The users that belong to the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'project_user', 'project_id', 'user_id');
+    }
 }
