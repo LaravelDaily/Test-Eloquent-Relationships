4a1c907fedb11613bec517c43d00f5e963ced2ee
Thananjeyan <thanjeys@gmail.com>
2022-02-15 16:32:43 +0530

    Relationship Finished

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Project;
+use App\Models\User;
+
 class ProjectController extends Controller
 {
     public function store(Request $request)
     {
         // TASK: Add one sentence to save the project to the logged-in user
         //   by $request->project_id and with $request->start_date parameter
+
+        $user= User::find(auth()->id());
+
+        $user->projects()->attach($request->project_id, ['start_date' => $request->start_date]);
+
 
         return 'Success';
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use App\Models\Task;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+
+        $this->call([
+            TaskSeeder::class,
+        ]);
     }
 }
