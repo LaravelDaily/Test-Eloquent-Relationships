dc35532e0c0fa636a472302ace8d2d1c562661ae
GianfrancoRocco <gianfrancorocco.business@gmail.com>
2022-02-28 16:06:11 -0300

    Green tests

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -15,4 +15,9 @@ class Task extends Model
     {
         return $this->belongsTo(User::class, 'users_id');
     }
+
+    public function attachments()
+    {
+        return $this->morphMany(Task::class, 'attachable');
+    }
 }
