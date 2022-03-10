715c76a39f1583036c8a7dcabdb505a519537ea0
Luca Di Fazio <highliuk@gmail.com>
2022-03-11 00:38:37 +0100

    Task 8 Solved

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -8,8 +8,13 @@ class ProjectController extends Controller
 {
     public function store(Request $request)
     {
-        // TASK: Add one sentence to save the project to the logged-in user
-        //   by $request->project_id and with $request->start_date parameter
+        $request->user()
+            ->projects()
+            ->syncWithoutDetaching([
+                $request->project_id => [
+                    'start_date' => $request->start_date
+                ]
+            ]);
 
         return 'Success';
     }
