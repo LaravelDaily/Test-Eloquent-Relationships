877850cdbd29a10afc0434019e57ffe5113d045f
David Villalobos <davidvillalobosvargas@gmail.com>
2022-04-28 18:53:59 -0600

    task8V2

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -10,9 +10,13 @@ class ProjectController extends Controller
     {
         // TASK: Add one sentence to save the project to the logged-in user
         //   by $request->project_id and with $request->start_date parameter
-        auth()->user()->projects()->create([
-            'name'=>$request->name
-        ]);
+        $request->user()
+            ->projects()
+            ->syncWithoutDetaching([
+                $request->project_id => [
+                    'start_date' => $request->start_date
+                ]
+            ]);
         
 
         return 'Success';
