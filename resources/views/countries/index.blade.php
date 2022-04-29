db1a182c82b248bb2b0f0c36deb6b11f0bd11f74
David Villalobos <davidvillalobosvargas@gmail.com>
2022-04-28 18:28:32 -0600

    task6v2

--- resources/views/countries/index.blade.php
+++ resources/views/countries/index.blade.php
@@ -1,5 +1,5 @@
 <ul>
     @foreach ($countries as $country)
-        <li>{{ $country->name }} (avg team size {{ $country->teams->avg() }})</li>
+        <li>{{ $country->name }} (avg team size {{ $country->teams_avg_size }})</li>
     @endforeach
 </ul>
