3f0944dd2ebebf72264a242d8f3dc4d150c0c84c
Preet Adhikari <blazin.preet@gmail.com>
2022-06-08 12:02:17 +0545

    All tasks completed

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -9,8 +9,18 @@ class CountryController extends Controller
     public function index()
     {
         // TASK: load the relationship average of team size
-        $countries = Country::all();
+        $team_avg_size = 0;
+        $count = 0;
+        $countries = Country::with('teams')->get();
+        foreach ($countries as $country){
+            $count = count($country->teams);
+            foreach ($country->teams as $team){
+                $team_avg_size += $team->size;
+            }
+        }
+        $team_avg_size = $team_avg_size / $count;
 
-        return view('countries.index', compact('countries'));
+
+        return view('countries.index', compact('countries','team_avg_size'));
     }
 }
