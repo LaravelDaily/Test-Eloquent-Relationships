64b62ac830ed41829a7afdca73e119afb16f6ec2
ab-azmi <azmi.kiva@gmail.com>
2022-07-15 23:27:08 +0700

    finished 1-5 test cases

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
@@ -15,4 +16,5 @@ class Task extends Model
     {
         return $this->belongsTo(User::class, 'users_id');
     }
+
 }
