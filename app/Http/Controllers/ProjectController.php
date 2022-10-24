6eb784f61cbf55d5c88e15893c92e9dc8bffd853
Khaled Ahmed <36667919+KhaledTolba@users.noreply.github.com>
2022-10-25 00:32:32 +0200

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -10,6 +10,9 @@ class ProjectController extends Controller
     {
         // TASK: Add one sentence to save the project to the logged-in user
         //   by $request->project_id and with $request->start_date parameter
+        User::find(Auth::id()) -> projects() -> attach($request -> project_id, [
+            'start_date' => $request->start_date
+        ]);
 
         return 'Success';
     }
