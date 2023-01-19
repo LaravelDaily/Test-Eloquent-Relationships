202dad69868d303b3851cafb967cf5dfb1c952a3
mshoubasi <mr8wolf@gmail.com>
2023-01-19 12:48:07 +0200

    eloquent-releationships-test-success

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class ProjectController extends Controller
 {
     public function store(Request $request)
     {
         // TASK: Add one sentence to save the project to the logged-in user
         //   by $request->project_id and with $request->start_date parameter
+        $user = User::find(auth()->user()->id);
+        $user->projects()->attach($request->project_id, ['start_date' => $request->start_date]);
 
         return 'Success';
     }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -15,4 +15,5 @@ class Comment extends Model
     {
         return $this->belongsTo(Task::class);
     }
+
 }
