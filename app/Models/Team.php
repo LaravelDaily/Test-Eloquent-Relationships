122270ee5d5f4ac5205a77853f38ee8631404818
Ezz690 <abouelez10@gmail.com>
2023-02-18 16:44:47 +0200

    1

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -14,7 +14,6 @@ class Team extends Model
     public function users()
     {
         // TASK: fix this by adding some extra code
-        return $this->belongsToMany(User::class);
+        return $this->belongsToMany(User::class)->withPivot(['position'])->withTimestamps();
     }
-
 }
