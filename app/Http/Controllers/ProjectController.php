e0ca129ac0746867a626bc32709d63bb24ecd62b
ZylalMinollari <zminollari@gmail.com>
2023-03-01 13:37:09 +0100

    Solved test eloquent relationships

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProjectController extends Controller
 {
@@ -11,6 +13,8 @@ class ProjectController extends Controller
         // TASK: Add one sentence to save the project to the logged-in user
         //   by $request->project_id and with $request->start_date parameter
 
+        $user = Auth::user();
+        $user->projects()->attach($request->project_id, ['start_date' => $request->start_date]);
         return 'Success';
     }
 }
