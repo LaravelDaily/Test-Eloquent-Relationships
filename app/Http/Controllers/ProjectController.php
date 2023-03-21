8fc6301947108d5b88fc01ceca1dfd7df54e7497
kairo <kairo.wageh@gmail.com>
2023-03-21 17:58:43 +0200

    finish tasks

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -11,6 +11,8 @@ class ProjectController extends Controller
         // TASK: Add one sentence to save the project to the logged-in user
         //   by $request->project_id and with $request->start_date parameter
 
+        auth()->user()->projects()->attach($request->project_id, ['start_date' => $request->start_date]);
+
         return 'Success';
     }
 }
