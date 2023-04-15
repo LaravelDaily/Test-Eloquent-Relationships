4846e81f37ad28edd1dbff9ee727f3fd6410ce1b
inam925 <97148236+inam925@users.noreply.github.com>
2023-04-15 08:28:21 +0500

    Update Role.php

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -14,6 +14,6 @@ class Role extends Model
     public function users()
     {
         // TASK: fix this by adding a parameter
-        return $this->belongsToMany(User::class, 'roles');
+        return $this->belongsToMany(User::class, 'users_roles');
     }
 }
