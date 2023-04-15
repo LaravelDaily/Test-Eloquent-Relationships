26be356250a79a94a71fe94e571b0bd4ecc573f7
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-04-15 12:12:16 +0200

    Task 6 ✅

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -6,11 +6,11 @@ use App\Models\Country;
 
 class CountryController extends Controller
 {
-    public function index()
-    {
-        // TASK: load the relationship average of team size
-        $countries = Country::all();
+	public function index()
+	{
+		// TASK: load the relationship average of team size ✅
+		$countries = Country::query()->withAvg('teams', 'size')->get();
 
-        return view('countries.index', compact('countries'));
-    }
+		return view('countries.index', compact('countries'));
+	}
 }
