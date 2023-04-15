c04eb6a8605d120df7c9a625edf6ea3257f91177
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-04-15 11:02:20 +0200

    Task 1 ✅

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,51 +10,51 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+	use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var string[]
+	 */
+	protected $fillable = [
+		'name',
+		'email',
+		'password',
+	];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for serialization.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password',
+		'remember_token',
+	];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+	/**
+	 * The attributes that should be cast.
+	 *
+	 * @var array
+	 */
+	protected $casts = [
+		'email_verified_at' => 'datetime',
+	];
 
-    public function tasks()
-    {
-        // TASK: fix this by adding a parameter
-        return $this->hasMany(Task::class);
-    }
+	public function tasks()
+	{
+		// TASK: fix this by adding a parameter ✅
+		return $this->hasMany(Task::class, 'users_id');
+	}
 
-    public function comments()
-    {
-        // TASK: add the code here for two-level relationship
-    }
+	public function comments()
+	{
+		// TASK: add the code here for two-level relationship
+	}
 
-    public function projects()
-    {
-        return $this->belongsToMany(Project::class)->withPivot('start_date');
-    }
+	public function projects()
+	{
+		return $this->belongsToMany(Project::class)->withPivot('start_date');
+	}
 }
