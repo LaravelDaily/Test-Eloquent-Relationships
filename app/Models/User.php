7fae705c11d445f98c3709b529a74d1038749ac9
inam925 <97148236+inam925@users.noreply.github.com>
2023-04-15 08:06:17 +0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,7 +51,7 @@ class User extends Authenticatable
     public function comments()
     {
         // TASK: add the code here for two-level relationship
-        return $this->hasMany(Comment::class, 'users_id');
+        return $this->hasManyThrough(Comment::class, Task::class, 'users_id');
     }
 
     public function projects()
