580c6df46e62099ba731b0c39869802c8d9bdaf8
Hussam Alhennawi <alhennawi.hussam@gmail.com>
2023-05-11 15:09:10 +0400

    Eloquent Relationship test Done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Task::factory(2)->create();
         // \App\Models\User::factory(10)->create();
     }
 }
