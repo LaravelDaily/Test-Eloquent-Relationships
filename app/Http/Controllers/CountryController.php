354fc0baf97b24ac1ed2503944a834c2bdc3734c
sothylor <panha.saloeun@gmail.com>
2023-05-29 22:20:07 +0700

    finished

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Country;
+use App\Models\Team;
 
 class CountryController extends Controller
 {
@@ -10,6 +11,17 @@ class CountryController extends Controller
     {
         // TASK: load the relationship average of team size
         $countries = Country::all();
+        foreach ($countries as $country){
+            $teams = Team::where('country_id', $country->id)->get();
+            $sum = 0;
+            $count = 0;
+            foreach($teams as $team){
+                $sum += $team->size;
+                $count += 1;
+            }
+            $country->teams_avg_size = $sum/$count;
+        }
+
 
         return view('countries.index', compact('countries'));
     }
--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class ProjectController extends Controller
 {
@@ -10,7 +14,9 @@ class ProjectController extends Controller
     {
         // TASK: Add one sentence to save the project to the logged-in user
         //   by $request->project_id and with $request->start_date parameter
-
+        User::find(auth()->id())->projects()->attach($request->project_id, [
+            'start_date' => $request->start_date
+        ]);
         return 'Success';
     }
 }
