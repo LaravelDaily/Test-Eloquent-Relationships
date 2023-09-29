9c301724593d64c5627055293357f2816266f5a2
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2023-09-29 14:48:54 +0700

    Test all pass

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -14,7 +14,7 @@ class Team extends Model
     public function users()
     {
         // TASK: fix this by adding some extra code
-        return $this->belongsToMany(User::class);
+        return $this->belongsToMany(User::class, 'team_user:position,created_at');
     }
 
 }
