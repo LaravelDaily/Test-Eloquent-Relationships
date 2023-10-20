5ecbd31f6cee2373d1ae775916243821980da3a8
exe544 <a.lashko97@gmail.com>
2023-10-20 16:14:54 +0300

    all tests solved

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProjectController extends Controller
 {
     public function store(Request $request)
     {
-        // TASK: Add one sentence to save the project to the logged-in user
-        //   by $request->project_id and with $request->start_date parameter
+
+        Auth::user()->projects()
+            ->attach(Project::find($request->project_id), ['start_date'=>$request->start_date]);
+
 
         return 'Success';
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -45,12 +46,12 @@ class User extends Authenticatable
     public function tasks()
     {
         // TASK: fix this by adding a parameter
-        return $this->hasMany(Task::class);
+        return $this->hasMany(Task::class, 'users_id');
     }
 
-    public function comments()
+    public function comments(): HasManyThrough
     {
-        // TASK: add the code here for two-level relationship
+        return $this->hasManyThrough(Comment::class, Task::class, 'users_id');
     }
 
     public function projects()
