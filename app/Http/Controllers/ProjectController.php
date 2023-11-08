15d75040dc547a21de083ef46937e46d7bbc5acb
azampanda <az.99.azam@gmail.com>
2023-11-08 10:52:35 +0800

    9 Tasks Completed

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -10,7 +10,14 @@ class ProjectController extends Controller
     {
         // TASK: Add one sentence to save the project to the logged-in user
         //   by $request->project_id and with $request->start_date parameter
-
+        auth()->user()->projects()->attach(
+            // target project
+            $request->project_id,
+            // additional data
+            [
+                'start_date' => $request->start_date,
+            ]
+        );
         return 'Success';
     }
 }
