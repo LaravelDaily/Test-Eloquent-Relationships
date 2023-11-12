efeb7e57973052413da38eca48de00c3e26306f8
Leandro <ramosdossantos.leandro@gmail.com>
2023-11-12 17:07:06 -0300

    All tests passed

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
+use Auth;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -10,7 +12,7 @@ class ProjectController extends Controller
     {
         // TASK: Add one sentence to save the project to the logged-in user
         //   by $request->project_id and with $request->start_date parameter
-
+        Auth::user()->projects()->attach($request->project_id, ['start_date' => $request->start_date]);
         return 'Success';
     }
 }
