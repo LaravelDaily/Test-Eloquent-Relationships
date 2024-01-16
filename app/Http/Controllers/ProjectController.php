75bf30fac06511c8610cb03a3c600f74f69c03a9
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2024-01-17 00:14:35 +0800

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -11,10 +11,6 @@ class ProjectController extends Controller
         // TASK: Add one sentence to save the project to the logged-in user
         //   by $request->project_id and with $request->start_date parameter
 
-        $user=auth()->user();
-        $user->projects()->find($request->project_id)->pivot->start_date=$request->start_date;
-
-        $user->push();
 
         
 
