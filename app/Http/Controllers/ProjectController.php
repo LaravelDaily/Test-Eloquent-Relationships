2d659be2a318597e94c93e200474f331fbb8d612
KiyaTilahun <kiyatilahun0@gmail.com>
2024-02-05 10:20:43 +0300

    eloquent relationship practice

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1,16 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function store(Request $request)
     {
-        // TASK: Add one sentence to save the project to the logged-in user
-        //   by $request->project_id and with $request->start_date parameter
-
+        auth()->user()->projects()->attach($request->project_id,['start_date' => $request->start_date]);
         return 'Success';
     }
 }
--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -13,6 +13,8 @@ class Attachment extends Model
 
     public function attachable()
     {
+        
+        return $this->morphTo();
         // TASK: fill in the code to make it work
     }
 }
