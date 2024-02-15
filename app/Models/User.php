20c61ef1f2671950b184dd3a969aeedb97d79db7
Ahmad Essam <72272968+ahmadessam5@users.noreply.github.com>
2024-02-15 17:37:14 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,7 +51,7 @@ class User extends Authenticatable
     public function comments()
     {
         // TASK: add the code here for two-level relationship
-        return $this->throughTasks()->hasComments();
+        return $this->hasManyThrough(Comment::class, Task::class);
     }
 
     public function projects()
