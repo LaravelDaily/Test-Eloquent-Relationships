e8068e0bdbd19ed67e07b6bfd0e6cab64ddf3dfc
Xuan <108394451+xuanxuan27@users.noreply.github.com>
2024-02-25 16:55:07 +0800

    Update CountryController.php

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -9,19 +9,7 @@ class CountryController extends Controller
     public function index()
     {
         // TASK: load the relationship average of team size
-        $countries = Country::all();
-
-        foreach ($countries as $country) {
-            $totalSize = 0;
-            $teamCount = 0;
-
-            foreach ($country->teams as $team) {
-                $totalSize += $team->size;
-                $teamCount++;
-            }
-
-            $country->teams_avg_size = $teamCount > 0 ? $totalSize / $teamCount : 0;
-        }
+        $countries = Country::withAvg('teams', 'size')->get();
 
         return view('countries.index', compact('countries'));
     }
